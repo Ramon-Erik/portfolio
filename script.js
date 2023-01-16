bfac8ccf37eaab15a0bc795678a9cd836c6dab7c
Ramon Erik <ramonerik29a@gmail.com>
2023-01-16 12:48:01 -0300

    everything ok
    index: added the name of the certificates and deleted unusual rows
    style: dangerous changes
    script: corrected the name of the image

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const nav = document.getElementsByTagName('nav')[0]
 const imgMainLogo = document.getElementById('main-logo')
 function scrolling(){
     nav.classList.toggle('scrolling', scrollY > 0)
-    imgMainLogo.src = 'imagens/logo-ramon-2.pngi'
+    imgMainLogo.src = 'imagens/logo-ramon-2.png'
 }
 
 window.addEventListener('scroll',  scrolling)
