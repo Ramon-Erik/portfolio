a5373c50c072daa8e33384f58be1057dab4783ad
Ramon Erik <ramonerik29a@gmail.com>
2023-01-23 21:21:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
 const nav = document.getElementsByTagName('nav')[0]
-const imgMainLogo = document.getElementById('main-logo')
-function scrolling(){
-    nav.classList.toggle('scrolling', scrollY > 0)
-    imgMainLogo.src = 'imagens/logo-ramon-2.png'
-}
-
-window.addEventListener('scroll',  scrolling)
